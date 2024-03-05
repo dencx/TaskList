c08a8274dd425465024bb7fdeef9791fca758ee6
Denys Bieliaiev <denisbieliaiev@gmail.com>
2024-03-06 00:44:53 +0100

    add prefix 'tasks' to the routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -67,18 +68,25 @@ $tasks = [
 ];
 
 Route::get('/', function () use ($tasks) {
+    return redirect()->route('tasks.index');
+});
+
+Route::get('/tasks', function () use ($tasks) {
     return view('index', [
         'tasks' => $tasks
     ]);
 })->name('tasks.index');
 
-Route::get('/{id}', function ($id) {
-    return 'One single task';
-
+Route::get('tasks/{id}', function ($id) use ($tasks) {
+    $task = collect($tasks)->firstWhere('id', $id);
+    if (!$task) {
+        abort(Response::HTTP_NOT_FOUND);
+    }
+    return view('show', ['task' => $task]);
 })->name('tasks.show');
 
 // Route::get('/xxx', function () {
-//     return 'Hello';
+//     return 'Hello';gi
 // })->name('hello');
 
 // Route::get('/halo', function () {
